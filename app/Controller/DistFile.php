b1193d07ac3c8c7721d0fc0341b174d015fc656f
Robert Lu <robberphex@gmail.com>
2017-05-08 10:54:36 +0800

    processWithoutProviders

--- app/Controller/DistFile.php
+++ app/Controller/DistFile.php
@@ -6,11 +6,12 @@ namespace App\Controller;
 use App\Config;
 use App\Controller;
 use App\Lib\Buffer;
+use GuzzleHttp\Client;
 use React\Dns\Resolver\Factory as DnsFactory;
 use React\EventLoop\LoopInterface;
 use React\Http\Request;
 use React\Http\Response;
-use React\HttpClient\Client;
+use React\HttpClient\Client as HttpClient;
 use React\HttpClient\Factory as ClientFactory;
 use React\HttpClient\Request as ClientRequest;
 use React\HttpClient\Response as ClientResponse;
@@ -26,6 +27,29 @@ class DistFile implements Controller
         $this->config = $config;
     }
 
+    public function processWithoutProviders($pkgname, $reference)
+    {
+        $storage_dir = $this->config->storage_dir;
+        $ret = false;
+
+        $client = new Client();
+        $res = $client->request('GET', 'https://packagist.org/p/' . $pkgname . '.json');
+        $json_content = json_decode($res->getBody(), true);
+        foreach ($json_content['packages'] as $pkg => $c) {
+            foreach ($c as $version => $c) {
+                $hash = $c['dist']['reference'];
+                if ($hash === $reference)
+                    $ret = true;
+                $url = $c['dist']['url'];
+                $meta_path = $storage_dir . '/p/' . $pkg . '/' . $hash;
+                if (!is_dir(dirname($meta_path)))
+                    mkdir(dirname($meta_path), 0777, true);
+                file_put_contents($meta_path, $url);
+            }
+        }
+        return $ret;
+    }
+
     public function action(Request $request, Response $response, $parameters)
     {
         $cache_file = $this->config->storage_dir . $request->getPath();
@@ -34,8 +58,12 @@ class DistFile implements Controller
             $comps1 = explode('/', $request->getPath());
             $comps2 = explode('.', $comps1[4]);
             $url_file = $this->config->storage_dir . '/p/' . $comps1[2] . '/' . $comps1[3] . '/' . $comps2[0];
-            if (!is_file($url_file)) {
+            if (
+                !is_file($url_file)
+                && !$this->processWithoutProviders($comps1[2] . '/' . $comps1[3], $comps2[0])
+            ) {
                 $response->writeHead(404);
+                $response->end();
                 return;
             }
             $origin_url = file_get_contents($url_file);
@@ -72,7 +100,7 @@ class DistFile implements Controller
         }
     }
 
-    static public function processRealData(ClientRequest $req, $startCallback, $callback, $endCallback, Client $client)
+    static public function processRealData(ClientRequest $req, $startCallback, $callback, $endCallback, HttpClient $client)
     {
         $processRedirect = function (ClientResponse $resp) use (&$client, &$processRedirect, $startCallback, $callback, $endCallback) {
             if (in_array($resp->getCode(), [301, 302, 304])) {
