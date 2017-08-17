840889d843c42798deecb2f47c29e4dc4e70d15d
Robert Lu <robberphex@gmail.com>
2017-08-18 01:48:03 +0800

    remove cache

--- app/Controller/DistFile.php
+++ app/Controller/DistFile.php
@@ -52,52 +52,42 @@ class DistFile implements Controller
 
     public function action(Request $request, Response $response, $parameters)
     {
-        $cache_file = $this->config->storage_dir . $request->getPath();
-
-        if (!file_exists($cache_file)) {
-            $comps1 = explode('/', $request->getPath());
-            $comps2 = explode('.', $comps1[4]);
-            $url_file = $this->config->storage_dir . '/p/' . $comps1[2] . '/' . $comps1[3] . '/' . $comps2[0];
-            if (
-                !is_file($url_file)
-                && !$this->processWithoutProviders($comps1[2] . '/' . $comps1[3], $comps2[0])
-            ) {
-                $response->writeHead(404);
-                $response->end();
-                return;
-            }
-            $origin_url = file_get_contents($url_file);
-
-            $resolverFactory = new DnsFactory();
-            $resolver = $resolverFactory->create('8.8.8.8', $this->loop);
-            $factory = new ClientFactory();
-            $client = $factory->create($this->loop, $resolver);
-
-            $request = $client->request('GET', $origin_url);
-
-            self::processRealData(
-                $request,
-                function (ClientResponse $resp) use ($response) {
-                    $response->writeHead(200, ['Cache-Control' => 'public, max-age=86400']);
-                },
-                function ($chunk) use ($response) {
-                    if (strlen($chunk) > 0)
-                        $response->write($chunk);
-                },
-                function ($buf) use ($response, $cache_file) {
-                    $response->end();
-                    if (!is_dir(dirname($cache_file)))
-                        mkdir(dirname($cache_file), 0777, true);
-                    file_put_contents($cache_file, $buf);
-                },
-                $client
-            );
-
-            $request->end();
-        } else {
-            $response->writeHead(200, ['Cache-Control' => 'public, max-age=86400']);
-            $response->end(file_get_contents($cache_file));
+        $comps1 = explode('/', $request->getPath());
+        $comps2 = explode('.', $comps1[4]);
+        $url_file = $this->config->storage_dir . '/p/' . $comps1[2] . '/' . $comps1[3] . '/' . $comps2[0];
+        if (
+            !is_file($url_file)
+            && !$this->processWithoutProviders($comps1[2] . '/' . $comps1[3], $comps2[0])
+        ) {
+            $response->writeHead(404);
+            $response->end();
+            return;
         }
+        $origin_url = file_get_contents($url_file);
+
+        $resolverFactory = new DnsFactory();
+        $resolver = $resolverFactory->create('8.8.8.8', $this->loop);
+        $factory = new ClientFactory();
+        $client = $factory->create($this->loop, $resolver);
+
+        $request = $client->request('GET', $origin_url);
+
+        self::processRealData(
+            $request,
+            function (ClientResponse $resp) use ($response) {
+                $response->writeHead(200, ['Cache-Control' => 'public, max-age=86400']);
+            },
+            function ($chunk) use ($response) {
+                if (strlen($chunk) > 0)
+                    $response->write($chunk);
+            },
+            function ($buf) use ($response, $cache_file) {
+                $response->end();
+            },
+            $client
+        );
+
+        $request->end();
     }
 
     static public function processRealData(ClientRequest $req, $startCallback, $callback, $endCallback, HttpClient $client)
