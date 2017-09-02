4c4aacf8adcc4cde1a82d8d95f2812b0aaaf154e
Robert Lu <robberphex@gmail.com>
2017-09-02 23:49:00 +0800

    支持返回provider*.json

--- bin/file.php
+++ bin/file.php
@@ -4,24 +4,31 @@ $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 1, 1) == '/')
     $uri = substr($uri, 1, -1);
 
-$parts = explode('/', $uri);
+if (substr($uri, -5) != '.json') {
+    $parts = explode('/', $uri);
 
 
-$vender = $parts[2];
-$name = $parts[3];
-$ref = explode('.', $parts[4])[0];
+    $vender = $parts[2];
+    $name = $parts[3];
+    $ref = explode('.', $parts[4])[0];
 
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-$url = $redis->hGet("$vender/$name", $ref);
+    $redis = new Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $url = $redis->hGet("$vender/$name", $ref);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_USERAGENT, 'LU');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-$res = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-header("Content-Type: $content_type", true, $code);
-echo $res;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'LU');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $res = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    header("Content-Type: $content_type", true, $code);
+    echo $res;
+} else {
+    $gz_path = __DIR__ . '/../storage/' . $uri . '.gz';
+    $content = file_get_contents($gz_path);
+    header("Content-Type: application/json");
+    echo gzuncompress($content);
+}
