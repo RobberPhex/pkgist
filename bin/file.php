61db85b93c184a052572c323c5030c0ddd8ae149
Robert Lu <robberphex@gmail.com>
2017-09-02 23:49:46 +0800

    忽略请求参数

--- bin/file.php
+++ bin/file.php
@@ -3,10 +3,10 @@
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 1, 1) == '/')
     $uri = substr($uri, 1, -1);
+$uri = strtok($uri, '?');
 
 if (substr($uri, -5) != '.json') {
     $parts = explode('/', $uri);
-
 
     $vender = $parts[2];
     $name = $parts[3];
@@ -14,7 +14,11 @@ if (substr($uri, -5) != '.json') {
 
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
-    $url = $redis->hGet("$vender/$name", $ref);
+    $url = $redis->hGet('file', "$vender/$name/$ref");
+    if (empty($url)) {
+        header("HTTP/1.1 404 Not Found");
+        die(404);
+    }
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -28,6 +32,11 @@ if (substr($uri, -5) != '.json') {
     echo $res;
 } else {
     $gz_path = __DIR__ . '/../storage/' . $uri . '.gz';
+    $gz_path = str_replace('%24', '$', $gz_path);
+    if (!file_exists($gz_path)) {
+        header("HTTP/1.1 404 Not Found");
+        die(404);
+    }
     $content = file_get_contents($gz_path);
     header("Content-Type: application/json");
     echo gzuncompress($content);
