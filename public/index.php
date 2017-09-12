50e5c35608bcbb9af6016bbba2ff2e1b34e146ab
Robert Lu <robberphex@gmail.com>
2017-09-12 17:35:51 +0800

    index.php doesn't process old style dist url

--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Predis\Client;
-use Symfony\Component\Yaml\Yaml;
-
 $uri = $_SERVER['REQUEST_URI'];
 if (substr($uri, 1, 1) == '/')
     $uri = substr($uri, 1, -1);
@@ -26,31 +23,6 @@ if (substr($uri, -5) != '.json') {
             header("HTTP/1.1 404 Not Found");
             die(404);
         }
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-        header("Content-Type: $content_type", true, $code);
-        header("Cache-Control: max-age=" . (24 * 60 * 60));
-        echo $res;
-    } elseif ($parts[1] == 'file') {
-        $vender = $parts[2];
-        $name = $parts[3];
-        $ref = explode('.', $parts[4])[0];
-
-        $path = __DIR__ . '/../config/app.yml';
-        $config = Yaml::parse(file_get_contents($path));
-        $redis = new Client($config['redis']);
-        $url = $redis->hGet('file', "$vender/$name/$ref");
-        if (empty($url)) {
-            header("HTTP/1.1 404 Not Found");
-            die(404);
-        }
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'LU');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        $res = curl_exec($ch);
         $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
         header("Content-Type: $content_type", true, $code);
